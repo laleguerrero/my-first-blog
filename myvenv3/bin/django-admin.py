0c5843e88c0ec88eb726227281c2874a4f0718db
Alejandra <ale.guerrero21@gmail.com>
2019-03-23 12:28:51 -0500

    My Django Girls app, first commit

---
+++ myvenv3/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/aleguerrero/Documents/tallerdjango/djangogirls/myvenv3/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
